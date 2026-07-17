67e3c740381809d95be1c23bba5ae093050dd715
NguyenNgocChien-01 <ngocchiien23@gmail.com>
2026-07-17 08:49:32 +0700

    feat(mailer)/done-6-template-otp

--- backend/src/mailer/config.py
+++ backend/src/mailer/config.py
@@ -15,6 +15,10 @@ EMAIL_TEMPLATE_CONFIGS: Final[dict[EmailTemplate, TemplateConfig]] = {
         "Reset your Lucidex password",
         "owner/reset_password_otp.html",
     ),
+    EmailTemplate.OWNER_LOGIN_OTP: (
+        "Your Lucidex login code",
+        "owner/login_otp.html",
+    ),
     EmailTemplate.ORGANIZATION_REGISTER_OTP: (
         "Activate your Lucidex organization account",
         "organization/register_otp.html",
@@ -22,5 +26,9 @@ EMAIL_TEMPLATE_CONFIGS: Final[dict[EmailTemplate, TemplateConfig]] = {
     EmailTemplate.ORGANIZATION_RESET_PASSWORD_OTP: (
         "Reset your Lucidex organization password",
         "organization/reset_password_otp.html",
+    ),
+    EmailTemplate.ORGANIZATION_LOGIN_OTP: (
+        "Your Lucidex organization login code",
+        "organization/login_otp.html",
     ),
 }

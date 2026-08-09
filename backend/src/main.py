9e39d5f3cedc90f4728eff73050b3ce84bd0436a
PhuPham <tranphupham2909@gmail.com>
2026-08-09 21:03:31 +0700

    new branch

--- backend/src/main.py
+++ backend/src/main.py
@@ -29,7 +29,7 @@ async def lifespan(_: FastAPI) -> AsyncIterator[None]:
 app = FastAPI(
     title=settings.APP_NAME,
     description="added datetime to admin request",
-    version="1.0.39",
+    version="1.0.46",
     lifespan=lifespan,
 )
 

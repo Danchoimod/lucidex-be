042ca132d4f20409421c474f16b0263502defe34
HuyHoang8324 <211481664+HuyHoang8324@users.noreply.github.com>
2026-08-12 02:55:36 +0000

    chore: bump version [skip ci]

--- backend/src/main.py
+++ backend/src/main.py
@@ -29,7 +29,7 @@ async def lifespan(_: FastAPI) -> AsyncIterator[None]:
 app = FastAPI(
     title=settings.APP_NAME,
     description="added datetime to admin request",
-    version="1.0.47",
+    version="1.0.48",
     lifespan=lifespan,
 )
 
